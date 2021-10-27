6464cd18569dfd990cb14e34061a48f48bbc7492
Matthew Simmons <matthewsimmonsdev@gmail.com>
2021-10-27 08:11:57 -0400

    Meets Expectations Complete

--- js/script.js
+++ js/script.js
@@ -1,30 +1,99 @@
 /*
 Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
+FSJS Project 2 - list Pagination and Filtering
 */
 
+/*
+This function displays 9 students per page with their information.  Pagination
+is included with the addPagination function.
+*/
+function showPage(list, page){
+   const studentsPerPage = 9;
+   const startIndex = page * studentsPerPage - studentsPerPage;
+   const endIndex = page * studentsPerPage;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+   for(let i = 0; i< list.length; i++){
+      if(i >= startIndex  && i < endIndex){
+         // Start of list elements
+         const studentLI = document.createElement(`li`);
+         studentLI.className = 'student-item cf';
+        
+         // student-details div
+         const studentDetails = document.createElement('div');
+         studentDetails.className = 'student-details';
+
+         const avatar = document.createElement('img');
+         avatar.className = 'avatar';
+         avatar.src = list[i].picture.large;
+         avatar.alt = 'Profile Picture';
+
+         const studentName = document.createElement('h3');
+         studentName.innerHTML = list[i].name.first + " " + list[i].name.last;
+
+         const studentEmail = document.createElement('span');
+         studentEmail.className = 'email';
+         studentEmail.innerHTML = list[i].email;
+         // End of student-details div
+
+         // Start joined-details div
+         const joinedDetails = document.createElement('div');
+         joinedDetails.className = 'joined-details';
+
+         const joinDate = document.createElement('span');
+         joinDate.className = 'date';
+         joinDate.innerHTML = `Joined ${list[i].registered.date}`
+         // End of joined-details div
+
+         studentList.appendChild(studentLI);
+         studentLI.appendChild(studentDetails);
+         studentDetails.appendChild(avatar);
+         studentDetails.appendChild(studentName);
+         studentDetails.appendChild(studentEmail);
+
+         studentLI.appendChild(joinedDetails);
+         joinedDetails.appendChild(joinDate);
+      }
+   }
+}
 
 
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+This function adds pagination which includes a loop to display a variable number of pages.
 */
+function addPagination(list){
+   const numberOfStudents = 9;
 
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
 
+   for(let i = 0; i <list.length / numberOfStudents; i++){
+      const buttonList = document.createElement('li');
+      const listButton = document.createElement('button');
+      listButton.innerHTML = i + 1;
+      
+      linkList.appendChild(buttonList);
+      buttonList.appendChild(listButton);
+   }
+   
+   const firstButton = linkList.firstChild.firstChild;
+   firstButton.className = 'active';
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+   linkList.addEventListener('click', (e) => {
+      const active = document.querySelector('.active');
+      const inactive = document.getElementsByTagName('button')
 
+      for(const button of inactive){
+         if(e.target === button){
+            active.classList.remove('active');
+            e.target.classList.add('active');
+            showPage(list, e.target.innerHTML);
+         }
+      }
+   });
 
+}
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-
-
-// Call functions
+// Function Calls
+showPage(data, 1);
+addPagination(data);
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is project number two for teamtreehouse.com
+It is an exercise in displaying data and separating it with pages.
